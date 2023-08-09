8a90beafacc9931a3ea476f8dcb9ff476167ba25
Ankit Kumar <ankit.kumar@samsung.com>
2023-08-09 16:43:12 +0530

    crc: pull required rocksoft crc64 apis from linux kernel
    Pull the required rocksoft crc64 apis from the linux kernel. This
    is required to generate and verify 64 bit guard tag for nvme backend
    of io_uring_cmd ioengine.
    
    Signed-off-by: Ankit Kumar <ankit.kumar@samsung.com>

--- crc/crc64.h
+++ crc/crc64.h
@@ -1,6 +1,10 @@
 #ifndef CRC64_H
 #define CRC64_H
 
+#include <linux/types.h>
+
 unsigned long long fio_crc64(const unsigned char *, unsigned long);
+
+__u64 fio_crc64_rocksoft_generic(__u64 crc, const void *p, unsigned int len);
 
 #endif
